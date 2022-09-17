cf8e226ba6f6ee0997ab081acbe5467fe2de0a55
professorCleytonMach1 <professor.cleyton.mach1@gmail.com>
2022-09-17 01:34:44 -0300

    Incluindo mais detalhes na primeira aula

--- assets/js/aula1.js
+++ assets/js/aula1.js
@@ -1,1 +1,7 @@
-console.log('Teste');
+/*
+Primeira brincadeira pegando um conteúdo do navegador
+*/
+
+let paragrafos = document.getElementsByTagName("p"); // Pegando todos os elementos <p> do documento
+console.log(paragrafos[0]) // Armazena num vetor e exibindo o primeiro <p> encontrado
+console.log(paragrafos[0].nodeName); // Exibindo o nome do nó do elemento 
