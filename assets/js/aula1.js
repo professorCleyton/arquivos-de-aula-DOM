8963dd64924b8cc4acde6b56e3e3198e34391a7c
Cleyton Oliveira <professor.cleyton.mach1@gmail.com>
2022-09-17 19:43:13 -0300

    Organizando melhor as informações

--- assets/js/aula1.js
+++ assets/js/aula1.js
@@ -1,7 +1,6 @@
 /*
 Primeira brincadeira pegando um conteúdo do navegador
 */
-
 let paragrafos = document.getElementsByTagName("p"); // Pegando todos os elementos <p> do documento
 console.log(paragrafos[0]) // Armazena num vetor e exibindo o primeiro <p> encontrado
 console.log(paragrafos[0].nodeName); // Exibindo o nome do nó do elemento 
