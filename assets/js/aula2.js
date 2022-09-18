b5e48d944512ebcfbdf35348bb1f7bf746dab805
Cleyton Oliveira <professor.cleyton.mach1@gmail.com>
2022-09-18 00:27:26 -0300

    Incluindo o JS dos exemplos aula 2

---
+++ assets/js/aula2.js
@@ -0,0 +1,36 @@
+
+/* DOCUMENT */
+// Propriedades
+document.body; // Retorna todos elementos dentro do body
+document.childElementCount; // Conta quantos elementos filhos(nodes) o elemento possui
+document.body.childElementCount; // outro exemplo
+document.children; // Busca os elementos filhos o elemento possui
+document.body.children; // Outro exemplo
+document.documentURI; // Recupera a URL da página que foi carregada
+
+//Métodos
+document.getElementById(); // Retorna a referência do elemento através do seu ID
+document.createElement(nomeDaTag);// Em um documento HTML, o método Document.createElement() cria o elemento HTML especificado
+document.getElementsByClassName(nomes);// Retorna um vetor de objetos com todos os elementos filhos que possuem o nome da classe dada
+
+//Nodes
+/*
+É    uma interface da qual diversos tipos do DOM herdam, e que permite que esses tipos sejam tratados de forma similar, 
+por exemplo, herdando os mesmos métodos ou sendo testados da mesma forma.
+*/
+
+//NodeList
+/*
+Objetos NodeList são coleções de nodos semelhantes aos objetos retornados 
+pelos métodos Node.childNodes e document.querySelectorAll().
+*/
+const myNodeList = document.querySelectorAll("p"); // Exemplo Nodelist de paragrafos
+
+// Element
+/*
+A interface Element é a classe base mais geral da qual todos os objetos em um Document herda
+*/
+const element = document.getElementById("idEspecifico");
+
+
+
