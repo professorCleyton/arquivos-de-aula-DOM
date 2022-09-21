b95387c277a5874d11a7d0876e6ab04578a7a04a
Cleyton Oliveira <professor.cleyton.mach1@gmail.com>
2022-09-21 20:43:39 -0300

    Conteúdo Aula 3

---
+++ assets/js/aula3.js
@@ -0,0 +1,17 @@
+let cpfUsuario = document.getElementById('cpf-usuario');
+let feedbackPositivo = document.getElementById('feedback-positivo');
+let feedbackNegativo = document.getElementById('feedback-negativo');
+let clicouNoBotao = () => console.log("Clicou!");
+let segundoBotao = document.getElementById("botao-valida");
+
+segundoBotao.onclick = () => {
+  let cpf = cpfUsuario.value;
+  
+  if(cpf.toString().length==11){
+    feedbackNegativo.setAttribute('hidden','true');
+    feedbackPositivo.removeAttribute('hidden');
+  }else{
+    feedbackPositivo.setAttribute('hidden','true');
+    feedbackNegativo.removeAttribute('hidden');
+  }
+};
